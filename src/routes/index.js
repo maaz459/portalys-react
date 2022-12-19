58352e61bf4ccd393171b1d00e547f06e62a7fbc
alighori <alighori267@gmail.com>
2022-12-19 10:30:57 -0800

    portalys page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,17 @@ import Home from "../pages/index";
 import Dashboard from "../pages/dashboard";
 import Events from "../pages/events/index";
 import CreateEvents from "../pages/events/create";
+import Portalys1 from "../pages/portalys/portalys1";
 const Routes = () => {
   return (
     <DomRoutes>
       <Route key="home" path="/" element={<Home />} />
       <Route key="organizer" path="organizer" element={<Home />} />
-
+      <Route
+        key="portalys1"
+        path="portalys1"
+        element={<Portalys1 />}
+      />
       <Route key="dashboard" path="dashboard" element={<Dashboard />} />
       <Route key="events"  path="events" element={<Events />} />
       <Route
@@ -20,6 +25,7 @@ const Routes = () => {
         path="resetpassword"
         element={<Home />}
       />
+        
     </DomRoutes>
   );
 };
